bd99ec20c2b2e5d9d852ccebd679b29854ea422b
João Pedro <pedrobarros2010@gmail.com>
2021-03-14 15:30:43 -0300

    refactor: creating src/ file

--- src/articles/Article.js
+++ src/articles/Article.js
@@ -3,17 +3,21 @@ const connection = require('../database/database.js')
 const Category = require('../categories/Category.js')
 
 const Article = connection.define('articles', {
-    title:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },slug:{
-        type: Sequelize.STRING,
-        allowNull: false
-    }, body: {
-        type: Sequelize.TEXT,
-        allowNull: false
-    }
+	title: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	slug: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	body: {
+		type: Sequelize.TEXT,
+		allowNull: false,
+	},
 })
+
+Article.sync({ force: true })
 
 Category.hasMany(Article)
 Article.belongsTo(Category)
